456a88ff13a3bd21c6b89f38d7b684e68f25e144
DDomiciano <dany_domiciano@hotmail.com>
2022-08-22 16:05:11 -0300

    Aula1

---
+++ src/main/java/br/edu/femass/model/Ponto.java
@@ -0,0 +1,62 @@
+package br.edu.femass.model;
+
+public class Ponto {
+
+    private Double x;
+
+    private Double y;
+
+    public Double getX(){
+        return this.x;
+    }
+
+    public Double getY(){
+        return this.y;
+    }
+
+    public Ponto(){
+        this.x = 0D;
+        this.y = 0D;
+    }
+
+    public Ponto (Double x, Double y){
+        this.x = x;
+        this.y = y;
+    }
+
+    public String getLocalizacao(){
+        if (this.x>0 && this.y>0){
+            return "1o Quadrante";
+        }
+
+        if (this.x<0 && this.y>0){
+            return "2o Quadrante";
+        }
+
+        if (this.x<0 && this.y<0){
+            return "3o Quadrante";
+        }
+
+        if (this.x>0 && this.y<0){
+            return "4o Quadrante";
+        }
+
+        if (this.x==0 && this.y!=0){
+            return "Eixo de Y";
+        }
+
+        if (this.x!=0 && this.y==0){
+            return "Eixo de X";
+        }
+        return "Origem";
+    }
+    public Double getDistancia(Ponto p){
+        return Math.sqrt(
+                Math.pow(p.getX()-this.x,2)
+                +Math.pow(p.getY()-this.y,2)
+
+        );
+
+    }
+
+}
